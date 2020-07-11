54cd536996f337e7e04a48dbac10f300cc0472a2
ishangika <ishangika93@gmail.com>
2020-07-11 17:39:40 +0530

    done edit profile

--- application/views/edit_profile.php
+++ application/views/edit_profile.php
@@ -25,41 +25,84 @@ $attributes = array('name' => 'login_data');
 
 echo form_open_multipart('login/do_upload', $attributes);
 ?>
-<div class="container">
+<div class="container edit_pro">
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">NAME :   </div>
+    <div class="col-md-9 col-sm-9">
     <div class="form-group">
                     <!-- <input type="hidden" name="id" class="form-control" value="<?php echo $user_detail->id;?>"> -->
-        NAME :   <input type="hidden" name="id" class="form-control" value="4">
+        <input type="hidden" name="id" class="form-control" value="4">
         <input type="text" name="name" class="form-control" value="<?php echo $user_detail->name;?>">
      </div>
-                 
-    <div class="form-group">
-       designation : <input type="text" name="designation" class="form-control" value="<?php echo $user_detail->designation;?>">
     </div>
-    <div class="form-group">
-    User Name : <input type="text" name="user_name" class="form-control" value="<?php echo $user_detail->user_name;?>">
     </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">designation : </div>
+    <div class="col-md-9 col-sm-9">
     <div class="form-group">
-    Department : <input type="text" name="department" class="form-control" value="<?php echo $user_detail->department;?>">
+       <input type="text" name="designation" class="form-control" value="<?php echo $user_detail->designation;?>">
     </div>
-    <div class="form-group">
-    Email : <input type="text" name="email" class="form-control" value="<?php echo $user_detail->email;?>">
     </div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">User Name : </div>
+    <div class="col-md-9 col-sm-9">
     <div class="form-group">
-    Date Of Birth : <input type="text" name="dob" class="form-control" value="<?php echo $user_detail->dob;?>">
+    <input type="text" name="user_name" class="form-control" value="<?php echo $user_detail->user_name;?>">
+    </div>
+    </div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">Department : </div>
+    <div class="col-md-9 col-sm-9">
+    <div class="form-group">
+    <input type="text" name="department" class="form-control" value="<?php echo $user_detail->department;?>">
+    </div>
+    </div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">Email : </div>
+    <div class="col-md-9 col-sm-9">
+    <div class="form-group">
+    <input type="text" name="email" class="form-control" value="<?php echo $user_detail->email;?>">
+    </div>
+    </div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">Date Of Birth : </div>
+    <div class="col-md-9 col-sm-9">
+    <div class="form-group">
+    <input type="text" name="dob" class="form-control" value="<?php echo $user_detail->dob;?>">
     
     </div>
+    </div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3 col-sm-3 feild">Profile Picutre : </div>
+    <div class="col-md-9 col-sm-9">
+    <div class="form-group">
+        <input type="file" name="image">
+    </div>
+    </div>
+    </div>
+
+    <div class="row">
+
+                <div class="form-group">
+                    <button class="btn btn-success" id="btn_upload" type="submit" style="margin-top: 40px;font-size: 30px;">Upload</button>
                 </div>
+    </div>
+    </div>
                  
                  
 
-                <div class="form-group">
-                   Profile Picutre <input type="file" name="image">
-                </div>
- 
-
-                <div class="form-group">
-                    <button class="btn btn-success" id="btn_upload" type="submit">Upload</button>
-                </div>
-
+                
+    
 </div>
 </div>
