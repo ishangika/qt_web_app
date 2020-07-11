d8c131c4409c01d45d232ed7bced9db7f42e9543
ishangika <ishangika93@gmail.com>
2020-07-11 22:18:06 +0530

    add new files

--- application/views/notification_list.php
+++ application/views/notification_list.php
@@ -1,4 +1,6 @@
+<p>Emojis</p>
 <?php
+
 if(!empty($imoj_array)){
 
 	foreach ($imoj_array as $titles) {
@@ -6,7 +8,24 @@ if(!empty($imoj_array)){
 		foreach ($titles['imoji'] as $title) {
 			// foreach ($title as $value) {
 				echo $title->imoji_count;
-				echo $title->image;
+				echo $title->imoji_ascii;
+			// }
+		}
+	
+	 }
+}
+
+?>
+<p>Comments</p>
+<?php
+if(!empty($comment_array)){
+
+	foreach ($comment_array as $titles) {
+		echo $titles['title'];
+		foreach ($titles['comment'] as $title) {
+			// foreach ($title as $value) {
+				echo $title->comment_count;
+				//echo $title->imoji_ascii;
 			// }
 		}
 	
