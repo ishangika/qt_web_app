20c7aa9df03847c0e1612d2ba19c663edb05329b
ishangika <ishangika93@gmail.com>
2020-07-11 23:50:59 +0530

    add

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -13,7 +13,41 @@
   <script src="<?php //echo base_url('application_resources/js/bootstrap.min.js')?>"></script>
   <script src="<?php echo base_url('application_resources/js/jquery-3.5.1.min.js')?>"></script>
 
- 
+ <!-- Menu -->
+ <div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					
+					</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+			</div>
+		</div>
+	</div>
+
+<!-- Menu	 -->
+
+
 <script>
 function openNote(evt, cityName) {
   var i, tabcontent, tablinks;
