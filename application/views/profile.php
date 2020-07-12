2051929e5ea9a1714505dc79176363214fb12b8d
ishangika <ishangika93@gmail.com>
2020-07-12 17:36:59 +0530

    add files

--- application/views/profile.php
+++ application/views/profile.php
@@ -7,42 +7,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Profile</title>
 	<?php include 'template/template.php';?>
 
-	
 </head>
 <body>
- <!-- Menu -->
- <div class="container">
-		<div class="row">
-			<div class="col-md-12 col-sm-12">
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					
-					</div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        
-      </ul>
-      
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-			</div>
-		</div>
-	</div>
-
-<!-- Menu	 -->
+ 
+<div class="dropdown">
+  <button class="dropbtn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+  <div class="dropdown-content">
+  <a href="#">Home</a>
+  <a href="http://localhost:8090/qt/index.php/login/edit_profile/4">Edit Profile</a>
+  <a href="http://localhost:8090/qt/index.php/forgot_password">Logout</a>
+  </div>
+</div> 
+ 
 <div id="container-fluid">
 	<div class="col-md-12 col-sm-12 nopadding">
 		<div class="profile_pic">
